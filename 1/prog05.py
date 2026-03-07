189b9acf8e8da24307a5843b50ba0a4621a4f44b
Josh112406 <quibaljosh@gmail.com>
2026-03-08 02:28:35 +0800

    add shorter function

--- 1/prog05.py
+++ 1/prog05.py
@@ -1,7 +1,11 @@
 #Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 #use .find() since it returns the index of the first occurence and if it returns 0, it starts with that, else False if it returns -1
-
+#long version, morpe readable
 def startswith(text: str, prefix: str):
     if text.find(prefix) == 0:
         return True
     return False
+
+#shorter version of the first function, less readable
+def startswith(text: str, prefix: str):
+    return True if text.find(prefix) == 0 else False
