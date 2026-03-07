b4f8b178947f4af840e217d0dc57c8b1a85f1551
Josh112406 <quibaljosh@gmail.com>
2026-03-08 01:23:38 +0800

    move file

---
+++ 1/prog01.py
@@ -0,0 +1,13 @@
+#Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+# rstrip() removes last character in argument list. skips if not included
+def rstrip(text: str, chars: str = " "):
+    #convert string to list
+    text_list = [x for x in text]
+    char_list = [x for x in chars]
+    while True:
+        if text_list[-1] in char_list:
+            text_list.pop(-1)
+        else:
+            break
+    print("".join(text_list))
+    
