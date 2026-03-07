55c2884d42779684a6f7b30494648eac5a0a6c89
Josh112406 <quibaljosh@gmail.com>
2026-03-08 01:56:46 +0800

    create islower function and testing

--- 1/prog04.py
+++ 1/prog04.py
@@ -1,1 +1,10 @@
 #Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+#use lowercase list
+def islower(text: str):
+    lowercase_list = [x for x in 'abcdefghijklmnopqrstuvwxyz']
+    for char in text:
+        if char.isalpha() and char in lowercase_list:
+            return True
+        return False
+    
+print(islower("Hfklsih"))
