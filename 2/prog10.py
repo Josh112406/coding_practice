00e8f7b3cd097b59d7458dfb84e15c6158f1992f
Josh112406 <quibaljosh@gmail.com>
2026-03-08 14:58:59 +0800

    fix order of logic in longer version. added short version

--- 2/prog10.py
+++ 2/prog10.py
@@ -1,4 +1,6 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+#shorter version
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 if num_1 < num_2:
@@ -7,7 +9,17 @@ if num_1 < num_2:
 elif num_1 > num_2:
     for i in range(num_1 - 1, num_2, -1):
         print(i)
+
+#longer version
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+if  num_1 + 1 == num_2 or num_2 + 1 == num_1:
+    print("Nothing in between")
+elif num_1 > num_2:
+    for i in range(num_1 - 1, num_2, -1):
+        print(i)
+elif num_1 < num_2:
+    for i in range(num_1, num_2 - 1):
+        print(i + 1)
 elif num_1 == num_2:
     print("The numbers are the same")
-else:
-    print("There is nothing in between")
