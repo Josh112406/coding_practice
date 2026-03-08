586eb1a064fb50f57b51f9f7b238623827a98453
Josh112406 <quibaljosh@gmail.com>
2026-03-08 14:15:37 +0800

    finalize prog04

---
+++ 2/prog04.py
@@ -0,0 +1,6 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#almost same as prog03
+
+num = float(input("Enter first number: "))
+num //= float(input("Enter second number: "))
+print(f"The quotient of the two numbers is: {int(num)}")
