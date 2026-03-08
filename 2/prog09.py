ec3caf2d1e304abbba73419c77f082e0c6135f14
Josh112406 <quibaljosh@gmail.com>
2026-03-08 15:39:19 +0800

    removed 0 in range

--- 2/prog09.py
+++ 2/prog09.py
@@ -1,5 +1,5 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
-for i in range(0, 101):
+for i in range(101):
     if i % 10 != 10 and i % 10 != 0:
         print(i)
