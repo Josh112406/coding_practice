037daec61fda7a64b18e53acdbcaf87ae18fdfa7
Josh112406 <quibaljosh@gmail.com>
2026-03-08 13:57:48 +0800

    added cleaner printout

--- 2/prog01.py
+++ 2/prog01.py
@@ -1,4 +1,4 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 num_1 = float(input("Enter first number: "))
 num_2 = float(input("Enter second number: "))
-print(num_1 if num_1 < num_2 else num_2)
+print(f"The smaller number is: {num_1 if num_1 < num_2 else num_2}")
