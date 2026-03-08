f70ddcac6a2860a2a2e2f6e804ec20be068c0f5d
Josh112406 <quibaljosh@gmail.com>
2026-03-08 14:05:14 +0800

    create longer version of function

--- 2/prog01.py
+++ 2/prog01.py
@@ -1,4 +1,14 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+#shorter version
 num_1 = float(input("Enter first number: "))
 num_2 = float(input("Enter second number: "))
 print(f"The smaller number is: {num_1 if num_1 < num_2 else num_2}")
+
+#longer version
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+if num_1 < num_2:
+    print(f"The smaller number is: {num_1}")
+else:
+    print(f"The smaller number is: {num_2}")
