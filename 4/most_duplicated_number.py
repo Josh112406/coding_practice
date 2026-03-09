af67b0e1a05620186e66327f2fd3975323ce66d1
Josh112406 <quibaljosh@gmail.com>
2026-03-09 13:21:44 +0800

    added function to list all number with same duplicate amount and fixed bug

--- 4/most_duplicated_number.py
+++ 4/most_duplicated_number.py
@@ -1,15 +1,19 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 nums = []
-duplicates = {}
+occurence = {}
+duplicates = []
 while True:
     try:
         nums.append(float(input("Enter number: ")))
     except ValueError:
         for i in range(len(nums)):
-            if nums[i] not in duplicates:
-                duplicates[nums[i]] = 1
+            if nums[i] not in occurence:
+                occurence[nums[i]] = 1
             else:
-                duplicates[nums[i]] = duplicates[nums[i]] + 1                
+                occurence[nums[i]] = occurence[nums[i]] + 1 
+        max_duplicate = max(occurence.values())
+        duplicates.extend([key for key, value in occurence.items() if value == max_duplicate])
         break
-print(f"Number with most duplicate is: {max(duplicates, key=duplicates.get)}")
+    
+print(f"Number/s with most duplicate is: {duplicates}")
