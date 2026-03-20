03e07e14164d23d491e7791c48882bb2f49e89e5
Josh112406 <quibaljosh@gmail.com>
2026-03-20 19:27:14 +0800

    improve memory usage

--- 6/find_unique_numbers.py
+++ 6/find_unique_numbers.py
@@ -1,12 +1,11 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
-nums = []
 duplicates = {}
 for i in range(10):
-    nums.append(int(input(f"Enter number {i + 1}: ")))
-    if nums[i] not in duplicates:
-        duplicates[nums[i]] = 1
+    num = int(input(f"Enter number {i + 1}: "))
+    if num not in duplicates:
+        duplicates[num] = 1
     else:
-        duplicates[nums[i]] = duplicates[nums[i]] + 1                
+        duplicates[num] = duplicates[num] + 1                
 
 print(f"Numbers without duplicates are: {[key for key, value in duplicates.items() if value == 1]}")
