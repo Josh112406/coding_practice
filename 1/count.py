3652457754808ca8ae38034bdae956f7d99f9a45
Josh112406 <quibaljosh@gmail.com>
2026-06-06 14:16:26 +0800

    fixed bug on overlapping occurences

--- 1/count.py
+++ 1/count.py
@@ -1,23 +1,16 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
-#works but slow
-def count(text: str, substring: str) -> int:
-    found = []
-    index = 0
-    for _ in range(len(text)):
-        string = text.find(substring, index) 
-        if string != -1:
-            found += [string]
-            index += 1
-    return len(set(found))
-
 #try to create more efficient algorithm
 def count(text: str, substring: str) -> int:
     count = 0
     len_substring = len(substring) 
-    for i in range(len(text)):
+    i = 0
+    while i <= len(text) - len_substring:
         if text[i : i + len_substring] == substring:
             count += 1
+            i += len_substring
     return count
 
+
+print(count("aaaa", "aa"))
 
