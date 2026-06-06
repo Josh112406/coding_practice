f88f0f5d05f3e3d2428b231ffad37ffc80fdcc23
Josh112406 <quibaljosh@gmail.com>
2026-06-06 14:23:12 +0800

    add handling if len of substring is 0

--- 1/index.py
+++ 1/index.py
@@ -2,9 +2,12 @@
 #same logic as prog08 but slightly tweaked
 #return index instead of incrementing
 def index(text: str, substring: str) -> int:
+    if not substring:
+        return 0
+    
     len_substring = len(substring)
     for i in range(len(text)):
         if text[i : i + len_substring] == substring:
             return i
-    raise IndexError
+    raise ValueError
         
