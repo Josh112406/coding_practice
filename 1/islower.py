d990b5830b0476e252cf8264e501db84190464ab
Josh112406 <quibaljosh@gmail.com>
2026-06-06 14:30:31 +0800

    fix logic error

--- 1/islower.py
+++ 1/islower.py
@@ -8,8 +8,12 @@ def islower(text: str) -> bool:
     
 #revised version, more readable and faster    
 def islower(text: str) -> bool:
+    has_alpha = False
     for char in text:
-        if char.isalpha() and not "a" <= char <= "z": #remove lowercase characters list comprehension for simplicity
-            return False
-    return True
-    
+        if char.isalpha(): #remove lowercase characters list comprehension for simplicity
+            has_alpha = True
+            if not "a" <= char <= "z":
+                return False
+    return has_alpha
+
+print(islower("123"))
