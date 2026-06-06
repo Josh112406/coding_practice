ce01fd718378ebdbdb3767969f489be3b77af398
Josh112406 <quibaljosh@gmail.com>
2026-06-06 13:20:05 +0800

    add handling if count is less than string length

--- 7/ljust.py
+++ 7/ljust.py
@@ -1,4 +1,7 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 def ljust(string: str, count: int, value: str = " ") -> str:
+    if count <= len(string):
+        return string
+    
     return string + (count - len(string)) * value
